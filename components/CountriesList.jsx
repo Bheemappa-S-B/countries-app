4dd656717ba1817d910b451771b8fee2b1839dac
Bheemappa <bheemappabaraker9@gmail.com>
2026-07-15 09:11:52 +0530

    changed api

--- components/CountriesList.jsx
+++ components/CountriesList.jsx
@@ -2,19 +2,22 @@ import React, { useEffect, useState } from 'react'
 // import countriesData from '../countriesData'
 import CountryCard from './CountryCard'
 import CountriesListShimmer from './CountriesListShimmer'
+import countries from '../countriesData'
 
 export default function CountriesList({ query }) {
-  const [countriesData, setCountriesData] = useState([])
+  // const [countriesData, setCountriesData] = useState([])
+
+const [countriesData] = useState(countries)
 
   // const [filteredData, setQuery] = useFilter(data, () => '')
 
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,region,subregion,capital,borders,tld,currencies,languages')
-      .then((res) => res.json())
-      .then((data) => {
-        setCountriesData(data)
-      })
-  }, [])
+  // useEffect(() => {
+  //   fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,region,subregion,capital,borders,tld,currencies,languages')
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setCountriesData(data)
+  //     })
+  // }, [])
 
   if (!countriesData.length) {
     return <CountriesListShimmer />
